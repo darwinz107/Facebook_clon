a006346d73d3086e2a3218179e8ec1c902f6c30a
darwinz107 <darwinalfredo88@gmail.com>
2025-07-23 12:15:54 -0500

    Another update

--- src/assets/connectionApi/Api.jsx
+++ src/assets/connectionApi/Api.jsx
@@ -1,6 +1,9 @@
 
 import { InferenceClient } from "@huggingface/inference";
 
+
+
+
 export const handlerApi = async (e,name,cellphone,email,password)=>{
 
     e.preventDefault();
@@ -40,12 +43,31 @@ export const registerApi = async (e,name,cellphone,email,password)=>{
 }
 
 
-export const chatIA = async(prompts) =>{
+export const getTokenH = async ()=>{
 
+    
+
+    const response = await fetch("http://localhost:3000/tokeHuggin/token",{
+        
+        method:"GET",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        
+    });
+
+    const data = await response.json()
+    return data
+
+}
+
+export const chatIA = async(prompts,token) =>{
+
+   
   const response = await  fetch("https://api-inference.huggingface.co/models/facebook/bart-large-cnn",{
     method:"POST",
        headers:{
-        "Authorization":`Bearer ${process.env.HF_TOKEN}`,
+        "Authorization":`Bearer ${token}`,
         "Content-Type": "application/json"
        },
        body: JSON.stringify({
@@ -70,10 +92,10 @@ export const chatIA = async(prompts) =>{
 }
 
 
-export const Deepseek = async (prompt) =>{
-const token = process.env.HF_TOKEN;
+export const Deepseek = async (prompt,token) =>{
 
-const client = new InferenceClient(token);
+
+const client = new InferenceClient(token.tokenH);
 
 const chatCompletion = await client.chatCompletion({
     provider: "fireworks-ai",
@@ -90,3 +112,27 @@ const chatCompletion = await client.chatCompletion({
 console.log(chatCompletion.choices[0].message);
 return chatCompletion.choices[0].message.content;
 }
+
+export const generateImg = async (prompt)=>{
+
+    const response = await fetch("http://localhost:3000/generate/image",{
+        
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify({
+          prompt:prompt
+        })
+        
+    });
+
+    const data = response.json()
+    return data
+
+}
+
+
+
+
+
